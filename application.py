fe180ce9fdc441db6d053dbdd21909dc6d401f10
kkito0726 <k.kito0726@gmail.com>
2022-08-26 22:43:28 +0900

    Revert "モジュールに分割"
    This reverts commit b9145a66e5d9a09834693836911175fa5711ff4e.

--- application.py
+++ application.py
@@ -2,12 +2,29 @@ from flask import Flask, make_response, jsonify, request
 from flask_cors import CORS
 import cv2, base64, io
 import numpy as np
-from stitch import stitch
 
 application = Flask(__name__)
 CORS(application) 
 
-
+def stitch(read_img, select_mode):
+    
+    if select_mode == "Scans":
+        stitcher = cv2.Stitcher.create(mode=cv2.Stitcher_SCANS)
+        print(f"{select_mode}モードで実行")
+        
+    else:
+        stitcher = cv2.Stitcher.create()
+        print(f"{select_mode}モードで実行")
+        
+    result = stitcher.stitch(read_img)
+    
+    if result[0]==0:
+        stitched = result[1]
+        
+        return stitched, result[0]
+    
+    elif result[0]==1:
+        return None, result[0]
 
 @application.route("/", methods=['GET'])
 def index():
